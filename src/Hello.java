c9761ecb5808d9549beb457376d219dd76327408
b1m4w3n <b1m4w3n@gmail.com>
2019-01-03 13:05:24 -0500

    Ate an apple or let's do shopping?

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,23 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 public class Hello{
+
+    LocalTime time = LocalTime.now();
+
+    void greeting(){
+        if(time.getHour() < 12){
+            System.out.println("Good Morning");
+        }else if(time.getHour() > 12 && time.getHour() < 18){
+            System.out.println("Good Afternoon. Just ate an apple.");
+        }else{
+            System.out.println("Good Evening. Let's do some shopping");
+        }
+    }
 
     public static void main(String[] args){
 
-        System.out.println("Hello hello 2019");
+        Hello hello = new Hello();
+        hello.greeting();
     }
 }
